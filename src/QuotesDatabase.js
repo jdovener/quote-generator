21635697423cf0cfb6c61e938d529bcc14500056
Jimmy <jdovener16@gmail.com>
2021-05-24 09:56:40 +0100

    first commit

---
+++ src/QuotesDatabase.js
@@ -0,0 +1,91 @@
+const quotes=[
+    {
+    "quote": "Sanity and Happiness are an impossible combination.",
+    "author": "Mark Twain"
+    }, {
+    "quote": "One's reach should always exceed one's grasp.",
+    "author": "Robert Browning"
+    }, {
+    "quote": "The placebo effect still works even if you know you are under the effect of a placebo.",
+    "author": "Unknown"
+    }, {
+    "quote": "People will forget what you said, people will forget what you did, but they will never forget how you made them feel.",
+    "author": "Maya Angelou"
+    }, {
+    "quote": "Remember how you felt when you wanted what you now have.",
+    "author": "Unknown"
+    }, {
+    "quote": "You would worry less about what people think of you if you realised how seldom they do.",
+    "author": "David Foster Wallace"
+    }, {
+    "quote": "Judge a man by only his ability to change.",
+    "author": "Unknown"
+    }, {
+    "quote": "A broken clock is right twice a day.",
+    "author": "Unknown"
+    }, {
+    "quote": "Stress is a choice.",
+    "author": "Unknown"
+    }, {
+    "quote": "Nearly all men can stand adversity, but if you want to test a man's character, give him power.",
+    "author": "Abraham Lincoln"
+    }, {
+    "quote": "Ownership is a concept.",
+    "author": "Unknown"
+    }, {
+    "quote": "A good quote is the amalgamation of a lifetime's worth of thinking.",
+    "author": "Unknown"
+    }, {
+    "quote": "To bake an apple pie from scratch, you must first invent the universe.",
+    "author": "Carl Sagan"
+    }, {
+    "quote": "The wise man knows that he knows nothing.",
+    "author": "Unknown"
+    }, {
+    "quote": "'Normal' is relative.",
+    "author": "Unknown"
+    }, {
+    "quote": "After the game, the king and pawn go into the same box.",
+    "author": "Italian Proverb"
+    },  {
+    "quote": "In moderation, everything. Including moderation.",
+    "author": "Oscar Wilde"
+    }, {
+    "quote": "To be a poet is a condition, not a profession.",
+    "author": "Robert Frost"
+    }, {
+    "quote": "I think that if you can turn off the mind and look only with the eyes, ultimately everything becomes abstract",
+    "author": "Ellsworth Kelly"
+    }, {
+    "quote": "You only live once but if you do it right once is enough.",
+    "author": "Mae West"
+    }, {
+    "quote": "Moral absolution inhibits our ability to learn from our mistakes.",
+    "author": "Unknown"
+    }, {
+    "quote": "Initiation invokes reciprication.",
+    "author": "Unknown"
+    }, {
+    "quote": "Don't let perfect be the enemy of good",
+    "author": "Unknown"
+    }, {
+    "quote": "It is possible to communicate without speaking and to speak without communicating.",
+    "author": "Unknown"
+    }, {
+    "quote": "A realist is what an optimist calls a pessimist.",
+    "author": "Unknown"
+    }, {
+    "quote": "Changing anything changes everything.",
+    "author": "Unknown"
+    }, {
+    "quote": "An eye for an eye makes the whole world blind.",
+    "author": "Mahatma Gandhi"
+    }, {
+    "quote": "A gold-plated sh*t sandwich is still a sh*t sandwich.",
+    "author": "A wise friend"
+    }, {
+    "quote": "Let he who is without sin cast the first stone.",
+    "author": "Jesus Christ"
+    }
+]
+export default quotes
