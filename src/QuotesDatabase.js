de37e093c6831ff3f38ccb1ff422f388b9bd3732
Jimmy <jdovener16@gmail.com>
2021-12-24 07:21:51 +0000

    orson welles

--- src/QuotesDatabase.js
+++ src/QuotesDatabase.js
@@ -96,5 +96,9 @@ const quotes=[
     "quote": "You shape and are shaped by your environment.",
     "author": "Unknown"
     }
+    {
+    "quote": "The enemy of art is the absence of limitations.",
+    "author": "Orson Welles"
+    }
 ]
 export default quotes
